9a9e0e7331c415d73ebae3a0b3e6e5ef6d02d99d
Bruxar <larancibia@utem.cl>
2024-09-13 18:04:22 -0300

    Refactor code to use HuggingFaceInstructEmbeddings for vectorstore

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://i.ibb.co/g3wRHxg/2a62c34e0d217a7aa14645ce114d84b3.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://i.ibb.co/g7D8Hf0/depositphotos-406452126-stock-illustration-user-icon-vector-people-icon.webp">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
